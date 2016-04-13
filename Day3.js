a168a44852bfa1f7cedb6b7d46c82bd148f5cddd
Blitzutu <Sky1460@comcast.net>
2016-04-12 20:52:01 -0700

    first commit

---
+++ Day3.js
@@ -0,0 +1,31 @@
+var bitwiseBuild = {
+  size: "large",
+  roomSize: 5, // to reference a string in an object i need to add "" so it will look like "Room Size"
+  Has_Staris: true,
+}
+
+console.log(bitwiseBuild.size)
+console.log(bitwiseBuild.roomSize); // to reference Room Size while written in a string write as console.log(bitwiseBuild[Room Size]);
+console.log(bitwiseBuild.Has_Staris);
+
+
+angular.module('myList', [])
+angular.module('myList').controller("Ctrl")
+
+angular.modulle('myList')
+angular.module('myList').controller('Ctrl'function($scope){
+  $scope.message = "Hello Skyler"
+  $scope.list =
+  [title: "Iron Man" genre: "Action"],
+  [title: "Lost" genre: "Drama"],
+  [title: "Juno" gerne: "Romantic Comedy"],
+})
+
+angular.module("myList". []);
+angular.modlue("myList").controller('Ctrl', function($scope)
+  $scope.welcome = "Hello World";
+  $scope.list =
+  [title: "Iron Man" gerne: "Action"],
+  [title: "Lost"  gerne: "Drama"],
+  [title: "Juno" gerne: "Romantic Comedy"]
+})
